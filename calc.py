74d95c74494644ca356531889475ff28af2934f5
james shao <jshao100@gmail.com>
2014-11-19 11:37:58 -0500

    ADD: modulo function

--- calc.py
+++ calc.py
@@ -33,6 +33,10 @@ def mult(a, b):
 def div(a, b):
     return a / b
 
+#modulo function
+def mod(a, b):
+    return a % b
+
 
 # -------------------------------------------------------- #
 
@@ -67,6 +71,8 @@ while (True):
             print "Product: ", mult(a, b)
         elif (op == "/"):
             print "Quotient: ", div(a, b)
+        elif (op == "%"):
+            print "Modulo: ", mod(a, b)
         else:
             print "Invalid operation..."
 
